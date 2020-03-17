37961a3562993a1ad83335a348d3d65fc6f02dbf
ashik8 <ashikshaheen187@gmail.com>
2020-03-17 20:28:54 +0600

    mistake fixed

--- roots_of_quadratic_equation.py
+++ roots_of_quadratic_equation.py
@@ -7,11 +7,11 @@ c= int(input('Enter value of c: '))
 D= b*b-4*a*c    #Discriminant
 
 if D>=0:
-    r1= ((-b + math.sqrt(D))/(2*a))  #Root 1
-    r2= ((-b - math.sqrt(D))/(2*a))  #Root 2
+    r1= (-b + math.sqrt(D))/(2*a)  #Root 1
+    r2= (-b - math.sqrt(D))/(2*a)  #Root 2
 
 else:   #Calculate Real and Imaginary Part
-    rp= b/(2*a)
+    rp= -b/(2*a)
     ip= math.sqrt(-D)/(2*a)
     r1= str(rp)+ ' +j'+str(ip)
     r2= str(rp)+ ' -j'+str(ip)
