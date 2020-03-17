459a5edb52b34031e7f124ae23fa26aed33b6bab
ashik8 <ashikshaheen187@gmail.com>
2020-03-17 20:38:07 +0600

    fixed mistake

--- check_prime_number.py
+++ check_prime_number.py
@@ -3,14 +3,17 @@ i=2
 
 n=int (input('Enter a number to check prime: '))
 
-while i<=n/2:
-    if n%i==0:
-        flag=0
-    
-    i=i+1
-
-if flag==0:
+if (n<2):
     print(n, ' is not a prime number.')
-
 else:
-    print(n, ' is a prime number.')
+    while i<=n/2:
+        if n%i==0:
+            flag=0
+        
+        i=i+1
+
+    if flag==0:
+        print(n, ' is not a prime number.')
+
+    else:
+        print(n, ' is a prime number.')
